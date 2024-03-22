1282bbe98e569f660f6808463895508a6235c701
Ian Turner <ianturner@Ians-MacBook-Air.local>
2024-03-22 22:17:52 +0000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ const App = () => {
   return (
     <div className="wrapper">
       <h1>👨🏻‍💻 Kitchen Sink</h1>
+      <ul>
+        <li>React</li>
+        <li>Emotion</li>
+        <li>Typescript</li>
+        <br />
+        <li>Babel</li>
+        <li>Webpack</li>
+        <br />
+        <li>React Testing Library</li>
+        <li>Playwright (visual testing)</li>
+      </ul>
       <Button type="button" variant="primary">
         This button does nothing
       </Button>
