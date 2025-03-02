23849a836ffc06dace6c708ff1bb271dfac116bc
Ian Turner <ianturner@Ians-MacBook-Air.local>
2025-03-02 23:07:12 +0000

    cleanup

--- src/client/components/input/Input.types.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default interface InputProps
-  /** allow all valid HTML attributes for an `input` element as props */
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
